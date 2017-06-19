a59c7e775a531a97734cb9d08ae9a9528513a321
Reza Ali <syed.reza.ali@gmail.com>
2017-06-19 08:34:43 -0700

    formated with clang and added post processing ability

--- src/Tiler.cpp
+++ src/Tiler.cpp
@@ -1,7 +1,7 @@
 #include "Tiler.h"
-#include "cinder/gl/gl.h"
-#include "cinder/app/App.h"
 #include "cinder/Rand.h"
+#include "cinder/app/App.h"
+#include "cinder/gl/gl.h"
 
 #include <algorithm>
 #include <sstream>
@@ -12,201 +12,232 @@ using namespace glm;
 using namespace std;
 
 Tiler::Tiler( int32_t imageWidth, int32_t imageHeight, int32_t tileWidth, int32_t tileHeight, ci::app::WindowRef window, bool alpha )
-: mImageWidth( (int)app::toPixels( (float)imageWidth ) ), mImageHeight( (int)app::toPixels( (float)imageHeight ) ), mWindowRef( window ), mDrawFn( nullptr ), mDrawBgFn( nullptr ), mDrawHudFn( nullptr ), mAlpha( alpha )
+	: mImageWidth( (int)app::toPixels( (float)imageWidth ) ), mImageHeight( (int)app::toPixels( (float)imageHeight ) ), mWindowRef( window ), mDrawFn( nullptr ), mDrawBgFn( nullptr ), mDrawHudFn( nullptr ), mAlpha( alpha )
 {
-    mWindowWidth = (int)app::toPixels( (float)mWindowRef->getWidth() );
-    mWindowHeight = (int)app::toPixels( (float)mWindowRef->getHeight() );
-    
-    mTileWidth = std::min( ( int32_t ) app::toPixels( (float)tileWidth ), mWindowWidth );
-    mTileHeight = std::min( ( int32_t ) app::toPixels( (float)tileHeight ), mWindowHeight );
+	mWindowWidth = (int)app::toPixels( (float)mWindowRef->getWidth() );
+	mWindowHeight = (int)app::toPixels( (float)mWindowRef->getHeight() );
 
-    mNumTilesX = ( int32_t ) ceil( mImageWidth / (float)mTileWidth );
-    mNumTilesY = ( int32_t ) ceil( mImageHeight / (float)mTileHeight );
-    
-    mCurrentTile = -1;
-    
-    if( mAlpha ) {
-        auto fmt = gl::Fbo::Format().samples( gl::Fbo::getMaxSamples() );
-        mFboRef = gl::Fbo::create( mWindowWidth, mWindowHeight, fmt );
-        mFboRef->bindFramebuffer();
-        gl::clear( ColorA( 0.0f, 0.0f, 0.0f, 0.0f ) );
-        mFboRef->unbindFramebuffer();
-    }
+	mTileWidth = std::min( (int32_t)app::toPixels( (float)tileWidth ), mWindowWidth );
+	mTileHeight = std::min( (int32_t)app::toPixels( (float)tileHeight ), mWindowHeight );
+
+	mNumTilesX = (int32_t)ceil( mImageWidth / (float)mTileWidth );
+	mNumTilesY = (int32_t)ceil( mImageHeight / (float)mTileHeight );
+
+	mCurrentTile = -1;
+
+	if( mAlpha ) {
+		mFboRef = setupFbo();
+		mPostFboRef = setupFbo();
+	}
 }
 
 bool Tiler::nextTile()
 {
-    if( mCurrentTile == mNumTilesX * mNumTilesY ) {
-        if( mAlpha ) {
-            mSurfaceRef->copyFrom( mFboRef->readPixels8u( Area( ivec2( 0 ) , ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
-        } else {
-            mSurfaceRef->copyFrom( mWindowRef->getRenderer()->copyWindowSurface( Area( ivec2( 0 ) , ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
-        }
-        mCurrentTile = -1;
-        return false;
-    }
-    
-    if( mCurrentTile == -1 ) {
-        mCurrentTile = 0;
-        if( mSurfaceRef && mSurfaceRef->getSize() != ivec2( mImageWidth, mImageHeight ) ) {
-            mSurfaceRef = Surface::create( mImageWidth, mImageHeight, mAlpha );
-        }
-        else {
-            mSurfaceRef = Surface::create( mImageWidth, mImageHeight, mAlpha );
-        }
-    }
-    else {
-        if( mAlpha ) {
-            mSurfaceRef->copyFrom( mFboRef->readPixels8u( Area( ivec2( 0 ) , ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
-        } else {            
-            mSurfaceRef->copyFrom( mWindowRef->getRenderer()->copyWindowSurface( Area( ivec2( 0 ) , ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
-        }
-    }
-    
-    int tileX = mCurrentTile % mNumTilesX;
-    int tileY = mCurrentTile / mNumTilesX;
+	if( mCurrentTile == mNumTilesX * mNumTilesY ) {
+		if( mAlpha ) {
+			auto fboRef = mDrawPostFn ? mPostFboRef : mFboRef;
+			mSurfaceRef->copyFrom( fboRef->readPixels8u( Area( ivec2( 0 ), ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
+		}
+		else {
+			mSurfaceRef->copyFrom( mWindowRef->getRenderer()->copyWindowSurface( Area( ivec2( 0 ), ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
+		}
+		mCurrentTile = -1;
+		return false;
+	}
 
-    int currentTileWidth = ( ( tileX == mNumTilesX - 1 ) && ( mImageWidth != mTileWidth * mNumTilesX ) ) ? ( mImageWidth % mTileWidth ) : mTileWidth;
-    int currentTileHeight = ( ( tileY == mNumTilesY - 1 ) && ( mImageHeight != mTileHeight * mNumTilesY ) ) ? ( mImageHeight % mTileHeight ) : mTileHeight;
+	if( mCurrentTile == -1 ) {
+		mCurrentTile = 0;
+		if( mSurfaceRef && mSurfaceRef->getSize() != ivec2( mImageWidth, mImageHeight ) ) {
+			mSurfaceRef = Surface::create( mImageWidth, mImageHeight, mAlpha );
+		}
+		else {
+			mSurfaceRef = Surface::create( mImageWidth, mImageHeight, mAlpha );
+		}
+	}
+	else {
+		if( mAlpha ) {
+			auto fboRef = mDrawPostFn ? mPostFboRef : mFboRef;
+			mSurfaceRef->copyFrom( fboRef->readPixels8u( Area( ivec2( 0 ), ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
+		}
+		else {
+			mSurfaceRef->copyFrom( mWindowRef->getRenderer()->copyWindowSurface( Area( ivec2( 0 ), ci::app::toPixels( mWindowRef->getSize() ) ), mCurrentArea.getHeight() ), Area( 0, 0, mCurrentArea.getWidth(), mCurrentArea.getHeight() ), mCurrentArea.getUL() );
+		}
+	}
 
-    mCurrentArea.x1 = tileX * mTileWidth;
-    mCurrentArea.x2 = mCurrentArea.x1 + currentTileWidth;
-    mCurrentArea.y1 = tileY * mTileHeight;
-    mCurrentArea.y2 = mCurrentArea.y1 + currentTileHeight;
-    
-    update();
-    mCurrentTile++;
-    return true;
+	int tileX = mCurrentTile % mNumTilesX;
+	int tileY = mCurrentTile / mNumTilesX;
+
+	int currentTileWidth = ( ( tileX == mNumTilesX - 1 ) && ( mImageWidth != mTileWidth * mNumTilesX ) ) ? ( mImageWidth % mTileWidth ) : mTileWidth;
+	int currentTileHeight = ( ( tileY == mNumTilesY - 1 ) && ( mImageHeight != mTileHeight * mNumTilesY ) ) ? ( mImageHeight % mTileHeight ) : mTileHeight;
+
+	mCurrentArea.x1 = tileX * mTileWidth;
+	mCurrentArea.x2 = mCurrentArea.x1 + currentTileWidth;
+	mCurrentArea.y1 = tileY * mTileHeight;
+	mCurrentArea.y2 = mCurrentArea.y1 + currentTileHeight;
+
+	update();
+	mCurrentTile++;
+	return true;
 }
 
 void Tiler::setMatricesWindowPersp( int screenWidth, int screenHeight, float fovDegrees, float nearPlane, float farPlane )
 {
-    CameraPersp cam( screenWidth, screenHeight, fovDegrees, nearPlane, farPlane );
-    setMatrices( cam );
+	CameraPersp cam( screenWidth, screenHeight, fovDegrees, nearPlane, farPlane );
+	setMatrices( cam );
 }
 
 void Tiler::setMatricesWindow( int32_t windowWidth, int32_t windowHeight )
 {
-    ortho( 0, (float)windowWidth, (float)windowHeight, 0, -1.0f, 1.0f );
+	ortho( 0, (float)windowWidth, (float)windowHeight, 0, -1.0f, 1.0f );
 }
 
 void Tiler::frustum( float left, float right, float bottom, float top, float nearPlane, float farPlane )
 {
-    mCurrentFrustumCoords = Rectf( vec2( left, top ), vec2( right, bottom ) );
-    mCurrentFrustumNear = nearPlane;
-    mCurrentFrustumFar = farPlane;
-    mCurrentFrustumPersp = true;
+	mCurrentFrustumCoords = Rectf( vec2( left, top ), vec2( right, bottom ) );
+	mCurrentFrustumNear = nearPlane;
+	mCurrentFrustumFar = farPlane;
+	mCurrentFrustumPersp = true;
 }
 
 void Tiler::ortho( float left, float right, float bottom, float top, float nearPlane, float farPlane )
 {
-    mCurrentFrustumCoords = Rectf( vec2( left, top ), vec2( right, bottom ) );
-    mCurrentFrustumNear = nearPlane;
-    mCurrentFrustumFar = farPlane;
-    mCurrentFrustumPersp = false;
+	mCurrentFrustumCoords = Rectf( vec2( left, top ), vec2( right, bottom ) );
+	mCurrentFrustumNear = nearPlane;
+	mCurrentFrustumFar = farPlane;
+	mCurrentFrustumPersp = false;
 }
 
 bool Tiler::getAlpha()
 {
-    return mAlpha; 
+	return mAlpha;
 }
 
-ci::Surface& Tiler::getSurface()
+ci::Surface &Tiler::getSurface()
 {
-    while ( nextTile() ) { }
-    return *mSurfaceRef;
+	while( nextTile() ) {
+	}
+	return *mSurfaceRef;
 }
 
 void Tiler::setDrawBgFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawBgFn )
 {
-    mDrawBgFn = drawBgFn;
+	mDrawBgFn = drawBgFn;
+}
+
+void reza::tiler::Tiler::setDrawPostFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawPostFn )
+{
+	mDrawPostFn = drawPostFn;
 }
 
 void Tiler::setDrawFn( const std::function<void()> &drawFn )
 {
-    mDrawFn = drawFn;
+	mDrawFn = drawFn;
 }
 
 void Tiler::setDrawHudFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawHudFn )
 {
-    mDrawHudFn = drawHudFn;
+	mDrawHudFn = drawHudFn;
 }
 
 void Tiler::setMatrices( const CameraPersp &camera )
 {
-    mCamera = camera;
-    float left, top, right, bottom, nearPlane, farPlane;
-    camera.getFrustum( &left, &top, &right, &bottom, &nearPlane, &farPlane );
-    
-    if( camera.isPersp() ) {
-        frustum( left, right, bottom, top, nearPlane, farPlane );
-    }
-    else {
-        ortho( left, right, bottom, top, nearPlane, farPlane );
-    }
+	mCamera = camera;
+	float left, top, right, bottom, nearPlane, farPlane;
+	camera.getFrustum( &left, &top, &right, &bottom, &nearPlane, &farPlane );
+
+	if( camera.isPersp() ) {
+		frustum( left, right, bottom, top, nearPlane, farPlane );
+	}
+	else {
+		ortho( left, right, bottom, top, nearPlane, farPlane );
+	}
 }
 
 void Tiler::update()
 {
-    if( mAlpha ) {
-        mFboRef->bindFramebuffer();
-        gl::clear( ColorA( 0.0f, 0.0f, 0.0f, 0.0f ) );
-    }
-    
-    float sx = (float) mCurrentArea.x1 / (float) mImageWidth;
-    float sy = 1.0 - (float) mCurrentArea.y1 / (float) mImageHeight;
-    float ex = (float) mCurrentArea.x2 / (float) mImageWidth;
-    float ey = 1.0 - (float) mCurrentArea.y2 / (float) mImageHeight;
-    
-    vec2 ul = vec2( sx, sy );
-    vec2 ur = vec2( ex, sy );
-    vec2 lr = vec2( ex, ey );
-    vec2 ll = vec2( sx, ey );
-    
-    if( mDrawBgFn ) {
-        gl::pushMatrices();
-        gl::pushViewport();
-        gl::viewport( mCurrentArea.getSize() );
-        mDrawBgFn( ul, ur, lr, ll );
-        gl::popViewport();
-        gl::popMatrices();
-    }
-    
-    float left = mCurrentFrustumCoords.x1 + mCurrentArea.x1 / (float)mImageWidth * mCurrentFrustumCoords.getWidth();
-    float right = left + mCurrentArea.getWidth() / (float)mImageWidth * mCurrentFrustumCoords.getWidth();
-    float top = mCurrentFrustumCoords.y1 + mCurrentArea.y1 / (float)mImageHeight * mCurrentFrustumCoords.getHeight();
-    float bottom = top + mCurrentArea.getHeight() / (float)mImageHeight * mCurrentFrustumCoords.getHeight();
-    
-    CameraPersp cam = mCamera;
-    gl::pushMatrices();
-    
-    gl::pushViewport();
-    gl::viewport( mCurrentArea.getSize() );
+	if( mAlpha ) {
+		mFboRef->bindFramebuffer();
+		gl::clear( ColorA( 0.0f, 0.0f, 0.0f, 0.0f ) );
+	}
 
-    gl::pushProjectionMatrix();
-    if( mCurrentFrustumPersp ) {
-        gl::setProjectionMatrix( glm::frustum( left, right, bottom, top, mCurrentFrustumNear, mCurrentFrustumFar ) );
-    } else {
-        gl::setProjectionMatrix( glm::ortho( left, right, bottom, top, mCurrentFrustumNear, mCurrentFrustumFar ) );
-    }
-    
-    gl::pushViewMatrix();
-    gl::setViewMatrix( cam.getViewMatrix() );
-    
-    if( mDrawFn ) {
-        mDrawFn();
-    }
+	float sx = (float)mCurrentArea.x1 / (float)mImageWidth;
+	float sy = 1.0 - (float)mCurrentArea.y1 / (float)mImageHeight;
+	float ex = (float)mCurrentArea.x2 / (float)mImageWidth;
+	float ey = 1.0 - (float)mCurrentArea.y2 / (float)mImageHeight;
 
-    gl::popViewMatrix();
-    gl::popProjectionMatrix();
-    gl::popViewport();
-    gl::popMatrices();
-    
-    if( mDrawHudFn ) {
-        mDrawHudFn( ul, ur, lr, ll );
-    }
-    
-    if( mAlpha ) {
-        mFboRef->unbindFramebuffer();
-    }
+	vec2 ul = vec2( sx, sy );
+	vec2 ur = vec2( ex, sy );
+	vec2 lr = vec2( ex, ey );
+	vec2 ll = vec2( sx, ey );
+
+	if( mDrawBgFn ) {
+		gl::pushMatrices();
+		gl::pushViewport();
+		gl::viewport( mCurrentArea.getSize() );
+		mDrawBgFn( ul, ur, lr, ll );
+		gl::popViewport();
+		gl::popMatrices();
+	}
+
+	float left = mCurrentFrustumCoords.x1 + mCurrentArea.x1 / (float)mImageWidth * mCurrentFrustumCoords.getWidth();
+	float right = left + mCurrentArea.getWidth() / (float)mImageWidth * mCurrentFrustumCoords.getWidth();
+	float top = mCurrentFrustumCoords.y1 + mCurrentArea.y1 / (float)mImageHeight * mCurrentFrustumCoords.getHeight();
+	float bottom = top + mCurrentArea.getHeight() / (float)mImageHeight * mCurrentFrustumCoords.getHeight();
+
+	CameraPersp cam = mCamera;
+	gl::pushMatrices();
+
+	gl::pushViewport();
+	gl::viewport( mCurrentArea.getSize() );
+
+	gl::pushProjectionMatrix();
+	if( mCurrentFrustumPersp ) {
+		gl::setProjectionMatrix( glm::frustum( left, right, bottom, top, mCurrentFrustumNear, mCurrentFrustumFar ) );
+	}
+	else {
+		gl::setProjectionMatrix( glm::ortho( left, right, bottom, top, mCurrentFrustumNear, mCurrentFrustumFar ) );
+	}
+
+	gl::pushViewMatrix();
+	gl::setViewMatrix( cam.getViewMatrix() );
+
+	if( mDrawFn ) {
+		mDrawFn();
+	}
+
+	gl::popViewMatrix();
+	gl::popProjectionMatrix();
+	gl::popViewport();
+	gl::popMatrices();
+
+	if( mDrawHudFn ) {
+		mDrawHudFn( ul, ur, lr, ll );
+	}
+
+	if( mAlpha ) {
+		mFboRef->unbindFramebuffer();
+	}
+
+	if( mDrawPostFn ) {
+		mFboRef->getColorTexture()->bind( 0 );
+		mPostFboRef->bindFramebuffer();
+		gl::clear( ColorA( 0.0f, 0.0f, 0.0f, 0.0f ) );
+		gl::pushMatrices();
+		gl::pushViewport();
+		gl::viewport( mCurrentArea.getSize() );
+		mDrawPostFn( ul, ur, lr, ll );
+		gl::popViewport();
+		gl::popMatrices();
+		mPostFboRef->unbindFramebuffer();
+	}
+}
+
+ci::gl::FboRef reza::tiler::Tiler::setupFbo()
+{
+	auto fmt = gl::Fbo::Format().samples( gl::Fbo::getMaxSamples() );
+	ci::gl::FboRef fboRef = gl::Fbo::create( mWindowWidth, mWindowHeight, fmt );
+	fboRef->bindFramebuffer();
+	gl::clear( ColorA( 0.0f, 0.0f, 0.0f, 0.0f ) );
+	fboRef->unbindFramebuffer();
+	return fboRef;
 }
--- src/Tiler.h
+++ src/Tiler.h
@@ -1,76 +1,104 @@
+/*
+Copyright(c) 2017 Reza Ali syed.reza.ali@gmail.com www.syedrezaali.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files(the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions :
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #pragma once
 
+#include "cinder/Area.h"
+#include "cinder/Camera.h"
 #include "cinder/Cinder.h"
-#include "cinder/app/Window.h"
-#include "cinder/app/AppBase.h"
-#include "cinder/gl/gl.h"
 #include "cinder/Exception.h"
 #include "cinder/Surface.h"
-#include "cinder/Camera.h"
-#include "cinder/Area.h"
+#include "cinder/app/AppBase.h"
+#include "cinder/app/Window.h"
+#include "cinder/gl/gl.h"
 #include "glm/glm.hpp"
 
-namespace reza { namespace tiler {
+namespace reza {
+namespace tiler {
 
 typedef std::shared_ptr<class Tiler> TilerRef;
 class Tiler {
-public:
-    static TilerRef create( glm::ivec2 imageSize, glm::ivec2 tileSize = glm::ivec2( 512, 512 ), ci::app::WindowRef window = ci::app::getWindow(), bool alpha  = false )
-    {
-        return TilerRef( new Tiler( imageSize.x, imageSize.y, tileSize.x, tileSize.y, window, alpha ) );
-    }
-    
-    static TilerRef create( int32_t imageWidth, int32_t imageHeight, int32_t tileWidth = 512, int32_t tileHeight = 512, ci::app::WindowRef window = ci::app::getWindow(), bool alpha = false )
-    {
-        return TilerRef( new Tiler( imageWidth, imageHeight, tileWidth, tileHeight, window, alpha ) );
-    }
-    
-    bool nextTile();
-    
-    int32_t getImageWidth() const { return mImageWidth; }
-    int32_t	getImageHeight() const { return mImageHeight; }
-    float getImageAspectRatio() const { return mImageWidth / (float)mImageHeight; }
-    ci::Area getCurrentTileArea() const { return mCurrentArea; }
-    ci::Surface& getSurface();
-    
-    void setDrawBgFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawBgFn );
-    void setDrawFn( const std::function<void()> &drawFn );
-    void setDrawHudFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawHudFn );
-    
-    void setMatrices( const ci::CameraPersp &camera );
-    void setMatricesWindowPersp( int screenWidth, int screenHeight, float fovDegrees, float nearPlane, float farPlane );
-    void setMatricesWindow( int32_t windowWidth, int32_t windowHeight );
-    void frustum( float left, float right, float bottom, float top, float nearPlane, float farPlane );
-    void ortho( float left, float right, float bottom, float top, float nearPlane, float farPlane ); 
-    
-    bool getAlpha(); 
-    
-protected:
-    Tiler( int32_t imageWidth, int32_t imageHeight, int32_t tileWidth = 512, int32_t tileHeight = 512, ci::app::WindowRef window = ci::app::getWindow(), bool alpha = false );
-    
-    void update();
-    
-    ci::app::WindowRef mWindowRef;
-    
-    int32_t mWindowWidth, mWindowHeight; 
-    int32_t mImageWidth, mImageHeight;
-    int32_t	mTileWidth, mTileHeight;
-    int32_t	mNumTilesX, mNumTilesY;
-    
-    int32_t	mCurrentTile;
-    ci::Area mCurrentArea;
-    ci::Rectf mCurrentFrustumCoords;
-    float mCurrentFrustumNear, mCurrentFrustumFar;
-    bool mCurrentFrustumPersp;
+  public:
+	static TilerRef create( glm::ivec2 imageSize, glm::ivec2 tileSize = glm::ivec2( 512, 512 ), ci::app::WindowRef window = ci::app::getWindow(), bool alpha = false )
+	{
+		return TilerRef( new Tiler( imageSize.x, imageSize.y, tileSize.x, tileSize.y, window, alpha ) );
+	}
 
-    ci::CameraPersp mCamera;
-    ci::SurfaceRef	mSurfaceRef;
+	static TilerRef create( int32_t imageWidth, int32_t imageHeight, int32_t tileWidth = 512, int32_t tileHeight = 512, ci::app::WindowRef window = ci::app::getWindow(), bool alpha = false )
+	{
+		return TilerRef( new Tiler( imageWidth, imageHeight, tileWidth, tileHeight, window, alpha ) );
+	}
 
-    bool mAlpha = false;
-    ci::gl::FboRef mFboRef = nullptr; 
-    std::function<void()> mDrawFn = nullptr;
-    std::function<void( glm::vec2,glm::vec2,glm::vec2,glm::vec2 )> mDrawBgFn = nullptr;
-    std::function<void( glm::vec2,glm::vec2,glm::vec2,glm::vec2 )> mDrawHudFn = nullptr;
+	bool nextTile();
+
+	int32_t getImageWidth() const { return mImageWidth; }
+	int32_t getImageHeight() const { return mImageHeight; }
+	float getImageAspectRatio() const { return mImageWidth / (float)mImageHeight; }
+	ci::Area getCurrentTileArea() const { return mCurrentArea; }
+	ci::Surface &getSurface();
+
+	void setDrawBgFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawBgFn );
+	void setDrawPostFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawPostFn );
+	void setDrawFn( const std::function<void()> &drawFn );
+	void setDrawHudFn( const std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> &drawHudFn );
+
+	void setMatrices( const ci::CameraPersp &camera );
+	void setMatricesWindowPersp( int screenWidth, int screenHeight, float fovDegrees, float nearPlane, float farPlane );
+	void setMatricesWindow( int32_t windowWidth, int32_t windowHeight );
+	void frustum( float left, float right, float bottom, float top, float nearPlane, float farPlane );
+	void ortho( float left, float right, float bottom, float top, float nearPlane, float farPlane );
+
+	bool getAlpha();
+
+  protected:
+	Tiler( int32_t imageWidth, int32_t imageHeight, int32_t tileWidth = 512, int32_t tileHeight = 512, ci::app::WindowRef window = ci::app::getWindow(), bool alpha = false );
+
+	void update();
+	ci::gl::FboRef setupFbo();
+
+	ci::app::WindowRef mWindowRef;
+
+	int32_t mWindowWidth, mWindowHeight;
+	int32_t mImageWidth, mImageHeight;
+	int32_t mTileWidth, mTileHeight;
+	int32_t mNumTilesX, mNumTilesY;
+
+	int32_t mCurrentTile;
+	ci::Area mCurrentArea;
+	ci::Rectf mCurrentFrustumCoords;
+	float mCurrentFrustumNear, mCurrentFrustumFar;
+	bool mCurrentFrustumPersp;
+
+	ci::CameraPersp mCamera;
+	ci::SurfaceRef mSurfaceRef;
+
+	bool mAlpha = false;
+	ci::gl::FboRef mFboRef = nullptr;
+	ci::gl::FboRef mPostFboRef = nullptr;
+	std::function<void()> mDrawFn = nullptr;
+	std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> mDrawBgFn = nullptr;
+	std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> mDrawPostFn = nullptr;
+	std::function<void( glm::vec2, glm::vec2, glm::vec2, glm::vec2 )> mDrawHudFn = nullptr;
 };
 
-} } // namespace cinder::gl
+} // namespace tiler
+} // namespace reza
